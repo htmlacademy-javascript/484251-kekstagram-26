9db94f12e961d9e9eea7c793899abd919d959ffb
Keks <keks@htmlacademy.ru>
2022-06-04 17:38:06 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,12 @@
+function getRandomInt(min, max) {
+  if (min >= max) {
+    return 0;
+  }
+  return Math.round(Math.random() * (max - min)) + min;
+}
+
+getRandomInt(100, 100);
+
+const checkLength = (checkedString, maxLength) => checkedString.length <= maxLength;
+
+checkLength('qwerty', 5);
