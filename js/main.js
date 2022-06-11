1f69387c74e81bb2a9d8baff21d27413e76de405
Alexey Belousov <asbelous@vk.com>
2022-06-11 10:48:05 +0700

    Добавляет функции для генерации данных

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Алексей',
+  'Ольга',
+  'Михаил',
+  'Галина',
+  'Иван',
+  'Алёна',
+  'Андрей',
+  'Вероника',
+  'Роман',
+  'Анастасия',
+];
+
+const SIMILAR_PHOTO_COUNT = 25;
+
+const commentIDs = [];
+const somePhotos = [];
+
 function getRandomPositiveInteger (a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -5,10 +32,57 @@ function getRandomPositiveInteger (a, b) {
   return Math.floor(result);
 }
 
-getRandomPositiveInteger(-45.98, -3.67);
-
 function checkStringLength (string, length) {
   return string.length <= length;
 }
 
-checkStringLength(HTMLAcademy, 15);
+//для линтера
+checkStringLength ('Урюрвкос', 10);
+
+function getCommentIDs () {
+  let id;
+  do {
+    id = getRandomPositiveInteger(1, 999);
+  } while (commentIDs.includes(id))
+  commentIDs.push(id);
+  return id;
+}
+
+function getRandomArrayElement (elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+}
+
+function createComments () {
+  return {
+    id: getCommentIDs(),
+    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+    message: getRandomArrayElement(MESSAGES),
+    names: getRandomArrayElement(NAMES),
+  };
+}
+
+function getComments () {
+  const comments = [];
+  for (let i = 0; i < 3; i++) {
+    comments.push(createComments());
+  }
+  return comments;
+}
+
+function createPhoto (id) {
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: 'Придумайте описание',
+    likes: getRandomPositiveInteger(15, 200),
+    comments: getComments(),
+  };
+}
+
+function getSimilarPhotos (count) {
+  for (let i = 1; i <= count; i ++) {
+    somePhotos.push(createPhoto(i));
+  }
+}
+
+getSimilarPhotos(SIMILAR_PHOTO_COUNT);
