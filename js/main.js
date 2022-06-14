88a22bc19581f718b5284181433eeba7fb0217dd
Alexey Belousov <asbelous@vk.com>
2022-06-14 11:31:23 +0700

    Добавляет перечисления и переписывает функции в стрелочные

--- js/main.js
+++ js/main.js
@@ -22,67 +22,77 @@ const NAMES = [
 
 const SIMILAR_PHOTO_COUNT = 25;
 
+const Avatar = {
+  MIN: 1,
+  MAX: 6,
+};
+
+const Like = {
+  MIN: 15,
+  MAX: 200,
+};
+
+const CommentID = {
+  MIN: 1,
+  MAX: 999,
+};
+
+const Comment = {
+  MIN: 1,
+  MAX: 20,
+};
+
 const commentIDs = [];
 const somePhotos = [];
 
-function getRandomPositiveInteger (a, b) {
+const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-}
+};
 
-function checkStringLength (string, length) {
-  return string.length <= length;
-}
+const checkStringLength = (string, length) => string.length <= length;
 
 //для линтера
 checkStringLength ('Урюрвкос', 10);
 
-function getCommentIDs () {
+const getCommentIDs = () => {
   let id;
   do {
-    id = getRandomPositiveInteger(1, 999);
+    id = getRandomPositiveInteger(CommentID.MIN, CommentID.MAX);
   } while (commentIDs.includes(id));
   commentIDs.push(id);
   return id;
-}
+};
 
-function getRandomArrayElement (elements) {
-  return elements[getRandomPositiveInteger(0, elements.length - 1)];
-}
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-function createComments () {
+function createComment () {
   return {
     id: getCommentIDs(),
-    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+    avatar: `img/avatar-${getRandomPositiveInteger(Avatar.MIN, Avatar.MAX)}.svg`,
     message: getRandomArrayElement(MESSAGES),
-    names: getRandomArrayElement(NAMES),
+    name: getRandomArrayElement(NAMES),
   };
-}
-
-function getComments () {
-  const comments = [];
-  for (let i = 0; i < 3; i++) {
-    comments.push(createComments());
-  }
-  return comments;
 }
 
 function createPhoto (id) {
   return {
-    id: id,
+    id,
     url: `photos/${id}.jpg`,
     description: 'Придумайте описание',
-    likes: getRandomPositiveInteger(15, 200),
-    comments: getComments(),
+    likes: getRandomPositiveInteger(Like.MIN, Like.MAX),
+    comments: Array.from({length: getRandomPositiveInteger(Comment.MIN, Comment.MAX)}, createComment),
   };
 }
 
-function getSimilarPhotos (count) {
+const getSimilarPhotos = (count) => {
   for (let i = 1; i <= count; i ++) {
     somePhotos.push(createPhoto(i));
   }
-}
+};
 
 getSimilarPhotos(SIMILAR_PHOTO_COUNT);
+
+// console.log(somePhotos);
